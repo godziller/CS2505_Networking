8c39e241ea3dd1bfa19fafb531e2cb7787dfa9f1
Darren Counihan <darren@counihan.ie>
2025-02-25 17:44:17 +0000

    adding server and client code

--- CS2505_lab1/client_2.py
+++ CS2505_lab1/client_2.py
@@ -5,10 +5,12 @@ from socket import *
 # Hints: AF_INET is used for IPv4 protocols, SOCK_STREAM is used for TCP 
 #<INSERT CALL TO CREATE THE SOCKET>
 client_socket = socket(AF_INET, SOCK_STREAM)
+host_name = gethostname()
+host_ip = gethostbyname(host_name)
 
 # set values for host 'localhost' - meaning this machine and port number 10000
 # the machine address and port number have to be the same as the server is using.
-server_address = ("locahost", 10000)
+server_address = (host_ip, 10000)
 # output to terminal some info on the address details
 print('connecting to server at %s port %s' % server_address)
 # Connect the socket to the host and port
--- CS2505_lab1/server_2.py
+++ CS2505_lab1/server_2.py
@@ -6,9 +6,11 @@ from socket import *
 # Hints: AF_INET is used for IPv4 protocols, SOCK_STREAM is used for TCP 
 
 server_socket = socket(AF_INET, SOCK_STREAM)
+host_name = gethostname()
+server_ip = gethostbyname(host_name)
 
 # set values for host 'localhost' - meaning this machine and port number 10000
-server_address = ("localhost", 10000)
+server_address = (server_ip, 10000)
 # output to terminal some info on the address details
 print('*** Server is starting up on %s port %s ***' % server_address)
 # Bind the socket to the host and port
